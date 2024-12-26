0ba18372275feb86020e90329cecf5b0fb30226d
omkumars31 <omkumar312003@gmail.com>
2024-12-26 13:36:35 +0530

    context API

---
+++ mini-Context/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1> React with the help of Context API</h1>
+      <Login/>
+      <Profile/>
+
+    </UserContextProvider>
+  )
+}
+
+export default App
